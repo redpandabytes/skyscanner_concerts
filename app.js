12939773ab7f2bd5d159e21dbf6880840296c02d
Klajdd <klajdkaraj14@gmail.com>
2017-01-28 17:02:26 +0000

    added artist.html and artist.js; updated app.js with artist.js

--- app.js
+++ app.js
@@ -35,3 +35,7 @@ app.get('/', function(req, res) {
 app.get('/test', function(req, res) {
   require( path.join(dir.CONTROLLER, 'test.js') )(req, res);
 });
+
+app.get('/artist', function(req, res) {
+  require( path.join(dir.CONTROLLER, 'artist.js') )(req, res);
+});
